609a1e0ec71f0327c980dab6edf6ceaa18e2907f
Manfred Rosskamp <manfred.rosskamp@gmx.de>
2015-11-10 18:33:35 +0100

    add sanitize

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 'use strict';
 
-angular.module('HSVApp', ['ngRoute'])
-  .config(function($routeProvider) {
+var app = angular.module('HSVApp', ['ngRoute', 'ngSanitize']);
+
+app.config(function($routeProvider) {
     $routeProvider
       .when('/', { templateUrl: 'start.html' })
       .when('/about', { template: 'Über uns ...' })
       .otherwise({ redirectTo: '/'});
-  })
-	.controller('ArticlesCtrl', function($scope, $sce) {
-		$scope.articles = [
-			{id: 1, title: "HSV ist Meister 2015", content: $sce.trustAsHtml("<p>blub <b><a href=\"http://www.google.de\">google</b></p>")},
-			{id: 2, title: "Letzes Spiel verschoben", content: $sce.trustAsHtml("<p>blub <b>bla</b></p>")},
-      {id: 3, title: "Letzes Spiel verschoben", content: $sce.trustAsHtml("<p>blub <b>bla</b></p>")},
-      {id: 4, title: "Letzes Spiel verschoben", content: $sce.trustAsHtml("<p>blub <b>bla</b></p>")},
-      {id: 5, title: "Letzes Spiel verschoben", content: $sce.trustAsHtml("<p>blub <b>bla</b></p>")},
-      {id: 6, title: "Letzes Spiel verschoben", content: $sce.trustAsHtml("<p>blub <b>bla</b></p>")},
-      {id: 6, title: "Letzes Spiel verschoben", content: $sce.trustAsHtml("<p>blub <b>bla</b></p>")},
-      {id: 6, title: "Letzes Spiel verschoben", content: $sce.trustAsHtml("<p>blub <b>bla</b></p>")},
-      {id: 7, title: "Letzes Spiel verschoben", content: $sce.trustAsHtml("<p>blub <b>bla</b></p>")}
-		]
+  });
+  
+app.controller('ArticlesCtrl', function($scope, $http) {
+    $http.get('articles.json').then(function(articlesResponse) {
+      $scope.articles = articlesResponse.data;
+    });
 	});
+  
