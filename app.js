723dbf5cd69a5e959e11b19991e492e39ab2e8ed
Manfred Rosskamp <manfred.rosskamp@gmx.de>
2015-11-11 08:15:12 +0100

    add ckeditor

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 'use strict';
 
-var app = angular.module('HSVApp', ['ngRoute', 'ngSanitize']);
+var app = angular.module('HSVApp', ['ngRoute', 'ngSanitize', 'ngCkeditor']);
 
 app.config(function($routeProvider) {
     $routeProvider
       .when('/', { templateUrl: 'start.html' })
+      .when('/edit_article/:articleId', { templateUrl: 'edit_article.html', controller: 'ArticleEditCtrl' })
       .when('/about', { template: 'Über uns ...' })
       .otherwise({ redirectTo: '/'});
   });
@@ -15,3 +16,21 @@ app.controller('ArticlesCtrl', function($scope, $http) {
     });
 	});
   
+app.controller('ArticleEditCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
+  $scope.$on("ckeditor.ready", function( event ) {
+                $scope.isReady = true;
+            });
+  $scope.editorContent = '<p>Hello</p>\n' + $routeParams.articleId;
+  
+  //$scope.save = function() {
+  //    $http.post('/examples/test.php', {
+  //        content: $scope.test
+  //    }).success(function() {
+  //        alert('Saved');
+  //    });
+  //}
+  
+  $scope.save = function() {
+      console.info($scope.editorContent, 'save');
+  }
+}]);
