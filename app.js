89ba3fbdcc74394e44f501db04590ce5eb908448
Manfred Rosskamp <manfred.rosskamp@gmx.de>
2016-01-02 17:32:56 +0100

    update

--- app.js
+++ app.js
@@ -2,21 +2,54 @@
 
 var app = angular.module('HSVApp', ['ngRoute', 'ngSanitize', 'ngCkeditor']);
 
+var backend = 'backend.php/';
+var tablePrefix = 'lm_';
+var backPrefix = backend + tablePrefix;
+
 app.config(function($routeProvider) {
     $routeProvider
-      .when('/', { templateUrl: 'start.html' })
+      .when('/', { templateUrl: 'views/Start.html' })
+      .when('/History', { templateUrl: 'views/History.html' })
+      .when('/Articles', { templateUrl: 'views/Articles.html' })
       .when('/edit_article/:articleId', { templateUrl: 'edit_article.html', controller: 'ArticleEditCtrl' })
-      .when('/Spieler', { templateUrl: 'Spieler.html' })
-      .when('/SpielerDetails/:spielerId', { templateUrl: 'SpielerDetails.html', controller: 'SpielerDetailsCtrl' })
-      .when('/about', { template: 'Über uns ...' })
+      .when('/Spieler', { templateUrl: 'views/Spieler.html' })
+      .when('/SpielerDetails/:spielerId', { templateUrl: 'views/SpielerDetails.html', controller: 'SpielerDetailsCtrl' })
+      .when('/Impressum', { templateUrl: 'views/Impressum.html' })
       .otherwise({ redirectTo: '/'});
   });
   
-app.controller('ArticlesCtrl', function($scope, $http) {
-    $http.get('articles.json').then(function(articlesResponse) {
-      $scope.articles = articlesResponse.data;
+app.filter("imageName", [function() {
+    return function(spielerName) {
+        return ("" + spielerName).replace(" ", "_");
+    }
+}]);
+
+
+app.controller('SpielerCtrl', function($scope, $http) {
+    $http.get('backend.php/jos_fussball_spieler').then(function (spielerResponse) {
+        $scope.spieler = php_crud_api_transform(spielerResponse.data)["jos_fussball_spieler"];
+        $scope.predicate = 'anzahlTore';
+        $scope.reverse = true;
+        $scope.order = function(predicate) {
+            $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
+            $scope.predicate = predicate;
+        }
     });
-	});
+});
+
+app.controller('SpielerDetailsCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
+    $http.get('backend.php/jos_fussball_spieler,jos_fussball_spieler_details?filter=id,eq,' + $routeParams.spielerId).then(function(spielerResponse) {
+        $scope.spieler = php_crud_api_transform(spielerResponse.data)["jos_fussball_spieler"][0];
+        $scope.spieler.details = $scope.spieler.jos_fussball_spieler_details[0];
+    });
+}]);
+
+app.controller('ArticlesCtrl', function ($scope, $http) {
+    var tableName = 'article';
+    $http.get(backPrefix + tableName).then(function (spielerResponse) {
+        $scope.articles = php_crud_api_transform(spielerResponse.data)[tablePrefix + tableName];
+    });
+});
   
 app.controller('ArticleEditCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
   $scope.$on("ckeditor.ready", function( event ) {
@@ -36,29 +69,3 @@ app.controller('ArticleEditCtrl', ['$scope', '$routeParams', function($scope, $r
       console.info($scope.editorContent, 'save');
   }
 }]);
-
-
-app.controller('SpielerCtrl', function($scope, $http) {
-    $http.get('api.php/jos_fussball_spieler').then(function(spielerResponse) {
-      $scope.spieler = php_crud_api_transform(spielerResponse.data)["jos_fussball_spieler"];
-      $scope.predicate = 'anzahlTore';
-      $scope.reverse = true;
-      $scope.order = function(predicate) {
-        $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
-        $scope.predicate = predicate;
-      }
-    });
-	});
-  
-app.controller('SpielerDetailsCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
-    $http.get('api.php/jos_fussball_spieler,jos_fussball_spieler_details?filter=id,eq,' + $routeParams.spielerId).then(function(spielerResponse) {
-      $scope.spieler = php_crud_api_transform(spielerResponse.data)["jos_fussball_spieler"][0];
-      $scope.spieler.details = $scope.spieler.jos_fussball_spieler_details[0];
-    });
-	}]);
-  
- app.filter("imageName", [function() {
-   return function(spielerName) {
-       return ("" + spielerName).replace(" ", "_");
-   }
- }]);
